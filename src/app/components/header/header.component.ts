5dfdb51a84642a5e12c82b08afb8614df1398e6c
zhiheng fan <fanzhihe@msu.edu>
2019-12-19 03:58:39 -0500

    second try deplot

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  
+  private resumelink: string;
   constructor() { }
 
   ngOnInit() {
+    this.resumelink = 'https://drive.google.com/open?id=18HjpTUV2HIhX2mB9cuO_8HvvZ0a1aU83';
+
+  }
+  goResume (){
+    window.location.href = this.resumelink;
   }
 
 }
--- src/app/pages/resume/resume.component.ts
+++ src/app/pages/resume/resume.component.ts
@@ -12,4 +12,7 @@ export class ResumeComponent implements OnInit {
   ngOnInit() {
   }
 
+  goResume () {
+    
+  }
 }
