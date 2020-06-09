ec8aa1ce11cb7fe9fd048167daed3bf6d19bf0c6
FanZhiheng <fanzhihe@msu.edu>
2020-06-09 11:15:49 +0800

    nothing

--- src/app/components/header/header.component.spec.ts
+++ src/app/components/header/header.component.spec.ts
@@ -13,6 +13,7 @@ describe('HeaderComponent', () => {
     .compileComponents();
   }));
 
+  
   beforeEach(() => {
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
